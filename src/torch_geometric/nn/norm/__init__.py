122d68c3ff75dca97daea5ccf6b004db1329b8d5
Prashant Pandey <prashant.prashn@gmail.com>
2020-05-22 11:34:37 -0700

    Removing pytorch related files.

--- src/torch_geometric/nn/norm/__init__.py
+++
@@ -1,9 +0,0 @@
-from .batch_norm import BatchNorm
-from .instance_norm import InstanceNorm
-from .graph_size_norm import GraphSizeNorm
-
-__all__ = [
-    'BatchNorm',
-    'InstanceNorm',
-    'GraphSizeNorm'
-]
