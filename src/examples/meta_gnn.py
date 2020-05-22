48880624eefc1ab2de97855131dfa5278615c20a
Prashant Pandey <prashant.prashn@gmail.com>
2020-05-21 21:59:58 -0700

    More progress towards getting the GCN working.

--- src/examples/meta_gnn.py
+++ src/examples/meta_gnn.py
@@ -12,8 +12,6 @@ import os.path as osp
 import torch
 import torch.nn.functional as F
 from torch_geometric.nn import GCNConv
-from torch_geometric.nn import GraphConv, TopKPooling
-from torch_geometric.nn import global_mean_pool as gap, global_max_pool as gmp
 
 from igraph import *
 from collections import defaultdict
@@ -22,6 +20,17 @@ from bidirectionalmap.bidirectionalmap import BidirectionalMap
 from torch_geometric.data import DataLoader
 from torch_geometric.data import Data
 from torch_geometric.data import InMemoryDataset
+
+def index_to_mask(index, size):
+    mask = torch.zeros((size, ), dtype=torch.bool)
+    mask[index] = 1
+    return mask
+
+def resize(l, newsize, filling=None):
+    if newsize > len(l):
+        l.extend([filling for x in range(len(l), newsize)])
+    else:
+        del l[newsize:]
 
 class Metagenomic(InMemoryDataset):
     r""" Assembly graph built over raw metagenomic data using spades.
@@ -66,7 +75,7 @@ class Metagenomic(InMemoryDataset):
     def process(self):
         assembly_graph_file = osp.join(self.raw_dir, self.raw_file_names[0])
         contig_paths = osp.join(self.raw_dir, self.raw_file_names[1])
-        taxa_file = osp.join(self.raw_dir, self.raw_file_names[2]) 
+        taxa_file = osp.join(self.raw_dir, self.raw_file_names[2])
         # Read assembly graph and node features from the file into arrays
         paths = {}
         segment_contigs = {}
@@ -192,15 +201,15 @@ class Metagenomic(InMemoryDataset):
         assembly_graph.simplify(multiple=True, loops=False, combine_edges=None)
         clusters = assembly_graph.clusters()
 
-
-
 ## Construct the feature vector from kraken2 output 
 #-------------------------------
 
         data_list = []
         node_features = []
+        node_taxon = []
         node_vector_map = defaultdict(set)
 
+        max_len = 0
         # Get tax labels from kraken2 output 
         with open(taxa_file) as file:
             line = file.readline()
@@ -209,22 +218,42 @@ class Metagenomic(InMemoryDataset):
                 if "C" in line:
                     strings = line.split("\t")
                     node_id = strings[1].split("_")[1]
-                    node_features.append(int(strings[2]))
+                    taxon_id = int(strings[2])
                     taxa = strings[4].split(" ")
+                    feature_list = []
                     for taxon in taxa:
                         if ":" in taxon:
-                            txid, abd = int(taxon.split(":")[0]), int(taxon.split(":")[1])
-                            if node_id not in node_vector_map:
-                                node_vector_map[node_id] = list([txid,abd])
-                            else:
-                                node_vector_map[node_id].append([txid,abd])
-                # node_features.append(node_vector_map[node_id])
+                            txid = int(taxon.split(":")[0])
+                            feature_list.append(txid)
+                node_taxon.append(taxon_id)
+                if max_len < len(feature_list):
+                    max_len = len(feature_list)
+                node_features.append(feature_list)
                 line = file.readline()
 
-        x = torch.tensor(node_features, dtype=torch.float)
-        edge_index = torch.tensor([source_nodes, dest_nodes], dtype=torch.long)
+        feature_vector = []
+        for node_list in node_features:
+            if len(node_list) < max_len:
+                resize(node_list, max_len, 0)
+            feature_vector.append(node_list)
 
-        data = Data(x=x, edge_index=edge_index)
+        x = torch.tensor(feature_vector, dtype=torch.float)
+        y = torch.tensor(node_taxon, dtype=torch.float)
+        edge_index = torch.tensor([source_nodes, dest_nodes], dtype=torch.long)
+        
+        train_size = int(node_count/3)
+        val_size = 1000
+        train_index = torch.arange(train_size, dtype=torch.long)
+        train_mask = index_to_mask(train_index, size=node_count)
+        val_index = torch.arange(train_size, train_size+val_size, dtype=torch.long)
+        val_mask = index_to_mask(val_index, size=node_count)
+        test_index = torch.arange(train_size+val_size, node_count, dtype=torch.long)
+        test_mask = index_to_mask(test_index, size=node_count)
+
+        data = Data(x=x, edge_index=edge_index, y=y)
+        data.train_mask = train_mask
+        data.val_mask = val_mask
+        data.test_mask = test_mask
         data_list.append(data)
 
         data, slices = self.collate(data_list)
@@ -233,53 +262,41 @@ class Metagenomic(InMemoryDataset):
     def __repr__(self):
         return '{}()'.format(self.name)
 
-
 class Net(torch.nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = GCNConv(dataset.num_node_features, 16)
-        # self.conv2 = GCNConv(16, dataset.num_classes)
+        self.conv1 = GCNConv(dataset.num_features, 16, cached=True)
+        self.conv2 = GCNConv(16, int(dataset.num_classes), cached=True)
+        # self.conv1 = ChebConv(data.num_features, 16, K=2)
+        # self.conv2 = ChebConv(16, data.num_features, K=2)
 
-    def forward(self, data):
-        x, edge_index = data.x, data.edge_index
+        self.reg_params = self.conv1.parameters()
+        self.non_reg_params = self.conv2.parameters()
 
-        x = self.conv1(x, edge_index)
-        x = F.relu(x)
+    def forward(self):
+        x, edge_index, edge_weight = data.x, data.edge_index, data.edge_attr
+        x = F.relu(self.conv1(x, edge_index, edge_weight))
         x = F.dropout(x, training=self.training)
-        x = self.conv2(x, edge_index)
-
+        x = self.conv2(x, edge_index, edge_weight)
         return F.log_softmax(x, dim=1)
 
 def train():
     model.train()
+    optimizer.zero_grad()
+    F.nll_loss(model()[data.train_mask], data.y[data.train_mask]).backward()
+    optimizer.step()
 
-    loss_all = 0
-    for data in train_loader:
-        data = data.to(device)
-        optimizer.zero_grad()
-        output = model(data)
-        label = data.y.to(device)
-        loss = crit(output, label)
-        loss.backward()
-        loss_all += data.num_graphs * loss.item()
-        optimizer.step()
-    return loss_all / len(train_dataset)
 
-def evaluate(loader):
+@torch.no_grad()
+def test():
     model.eval()
+    logits, accs = model(), []
+    for _, mask in data('train_mask', 'val_mask', 'test_mask'):
+        pred = logits[mask].max(1)[1]
+        acc = pred.eq(data.y[mask]).sum().item() / mask.sum().item()
+        accs.append(acc)
+    return accs
 
-    predictions = []
-    labels = []
-
-    with torch.no_grad():
-        for data in loader:
-
-            data = data.to(device)
-            pred = model(data).detach().cpu().numpy()
-
-            label = data.y.detach().cpu().numpy()
-            predictions.append(pred)
-            labels.append(label)
 
 # Sample command
 # -------------------------------------------------------------------
@@ -329,14 +346,39 @@ logger.info("Dataset: "+data_name)
 
 logger.info("MetaGNN started")
 
-logger.info("Constructing the assembly graph")
+logger.info("Constructing the assembly graph and node feature vectors")
 
 dataset = Metagenomic(root=input_dir, name=data_name)
-# print(dataset[0].num_nodes)
+data = dataset[0]
+print(data)
 # print(dataset[0].x)
 # print(dataset[0].edge_index)
 
+logger.info("Graph construction done!")
+elapsed_time = time.time() - start_time
+logger.info("Elapsed time: "+str(elapsed_time)+" seconds")
 
+logger.info("Training model")
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model, data = Net().to(device), data.to(device)
+optimizer = torch.optim.Adam([
+    dict(params=model.reg_params, weight_decay=5e-4),
+    dict(params=model.non_reg_params, weight_decay=0)
+], lr=0.01)
+
+best_val_acc = test_acc = 0
+for epoch in range(1, 20):
+    train()
+    train_acc, val_acc, tmp_test_acc = test()
+    if val_acc > best_val_acc:
+        best_val_acc = val_acc
+        test_acc = tmp_test_acc
+    log = 'Epoch: {:03d}, Train: {:.4f}, Val: {:.4f}, Test: {:.4f}'
+    print(log.format(epoch, train_acc, best_val_acc, test_acc))
+
+"""
+dataset = dataset
 train_size = int(dataset[0].num_nodes/3)
 val_size = 1000
 dataset = dataset.shuffle()
@@ -349,7 +391,7 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model = Net().to(device)
 optimizer = torch.optim.Adam(model.parameters(), lr=0.005)
 crit = torch.nn.BCELoss()
-batch_size=60
+batch_size=512
 train_loader = DataLoader(train_dataset, batch_size=batch_size)
 val_loader = DataLoader(val_dataset, batch_size=batch_size)
 test_loader = DataLoader(test_dataset, batch_size=batch_size)
@@ -364,6 +406,7 @@ for epoch in range(1):
     test_acc = evaluate(test_loader)
     print('Epoch: {:03d}, Loss: {:.5f}, Train Auc: {:.5f}, Val Auc: {:.5f}, Test Auc: {:.5f}'.
           format(epoch, loss, train_acc, val_acc, test_acc))
+"""
 
 """
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
